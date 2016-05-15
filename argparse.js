4e6d556569efd1b86f1cb0f38ad1f772b0587a7b
splitwire <clifford.kasper@gmail.com>
2016-05-14 20:15:05 -0500

    Updated to a Class structure.  This still does not work.

--- argparse.js
+++ argparse.js
@@ -1,86 +1,71 @@
-var args = {};
-var opts = [];
+'use strict';
 
-function arg_parse() {
-  
-  if( opts.length == 0 ) { return false; }
-  
-  var type_def = { 
-    's':  { 'type': 'String', 'eval': typeof 's'}, 
-    'i':  { 'type': 'Integer', 'eval': typeof 1}, 
-  };
-  
-  var arg_list = config.system.args;
-  arg_list.shift();
-
-  opts.forEach( function(opt, i ) {
-
-    // PARSE OUT ARGUMENTS TO ARGS OBJ
-    opt.split('|').forEach( function(o, i) { args[o] = false; });
-    
-    if( opt.indexOf('=') !== -1) {
-      var arg_type = opt.split('=')[1];
-      
-      // VERIFY ARGUMENT TYPE IS ALLOWED
-      if( Object.keys(type_def).indexOf( arg_type ) == -1 ) {
-        
-        //THROW ERROR
-        console.log( 'ERROR: "' + arg_type + '" is not an allowed input type.');
-        phantom.exit();
-        
-      }
-      
-      var value = undefined;
-      var just_arg_list = opt.split('=')[0]
-      just_arg_list.split('|').forEach( function(o, i) { 
-        
-        var idx = arg_list.indexOf( o );        
-        if( idx !== -1 ) { value = arg_list[ idx + 1 ]; }
-        
-      });
-
-            
-      if ( Object.keys( args ).indexOf( value ) !== -1 ) {
-        console.log( 'ERROR: There was an imporperly defined argument.');
-        phantom.exit();
-      }
-      
-      
-      just_arg_list.split('|').forEach( function(o, i) {
-        
-        if( type_def[arg_type]['type'] == 'String' ) {
-          
-          args[o] = value;
-          
-        } else {
-
-          if( isNaN(value) ) {
-            
-            console.log( 'ERROR: "' + o + '" must be an integer.');
-            phantom.exit();
-            
-          } else {
-            
-            args[o] = parseInt(value);
-            
-          }
-          
-        }
-
-      });
-      
-    } else {
-      
-      Object.keys(args).forEach( function(a, i ) {
-        
-        if( arg_list.indexOf( a ) !== -1 ) {
-          args[a] = true;
-        }
-        
-      })
+class argparse {
+    constructor( opts ){
+        this.parseOpts( opts );
     }
     
-  });
-  
+    
+    parseOpts( opts ) {
+        var opt_list = {};
+        var args = {};
+        
+        // PARSE OUT ALL POSSIBLE OPTIONS
+        opts.forEach( function(a) {
+            var tmp_split = a.split('|');
+            opt_list[ tmp_split[0] ] = {};
+            
+            tmp_split.forEach(function(aa) {
+                opt_list[ tmp_split[0] ][ aa.split('=')[0] ] = a.split('=')[1];
+            }); 
+        });
+        
+        
+        console.log( opt_list );
+        console.log( args );
+        return;
+        
+        
+        Object.keys(opt_list).forEach(function(a) {
+            var value;
+            console.log( opt_list[a]);
+            
+            Object.keys( opt_list[a] ).forEach(function(b) {
+                console.log( b );
+                
+                if( OPTIONS.indexOf(b) !== -1 ) {
+                    value = true;
+                    console.log(b);
+                } 
+            });
+            
+        });
+        
+        return;
+        
+        Object.keys(opt_list).forEach(function(o) {
+            if( OPTIONS.indexOf( o ) !== -1 ) { args[o] = true }    
+        });
+        
+        console.log( opt_list );
+ 
+        
+        console.log( args );
+        return;
+    }
+    
+    
+
 }
-                 
+
+var opts = [
+    'v|verbose',
+    'n|name=s',
+    'h|help',
+    'int|integer=i',
+    'bugger'
+];
+
+var OPTIONS = ['v', 'name', 'int', 'bugger'];
+
+var argP = new argparse( opts );
