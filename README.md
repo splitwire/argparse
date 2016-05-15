f74c4868d535ba05cfed7a39ab227f32999ddc1f
splitwire <clifford.kasper@gmail.com>
2016-05-15 13:53:51 -0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,74 @@
 # argparse
-This is an argument parser for PhantomJS.
+This is an argument parser for PhantomJS.  It is very similar to Perl's Getopt::Long module.
+
+USAGE:
+
+First you must inject the file into PhantomJS using phantom.injectJs('argparse.js');
+
+Example:
+var injectedJS = phantom.injectJs('argparse.js');
+
+Secondly you have to specify your options within an array.
+
+Example:
+var opts = [
+    'f|file=s',
+    'v|verbose',
+    'r'
+]
+
+The array must consist of the options you want passed when PhantomJS is ran.  Each pipe '|' separates an alias for the first string before the pipe.  In this example the 'f' is the main argument but it also has an alias 'file' which can be used interchangably for 'f'.  
+
+For instance the user can run the following:
+    phantomjs file.js f validation.js OR phantomjs file.js file validation.js
+    
+The character directly following a '=' will indicate the type of input which is required to follow the argument on the command line when running PhantomJS.  Currently the only two options are 's' for string and 'i' for integer.  An integer that follows an argument will be treated as a string if the opts indicates a string input is required, however, if a string follows an argument that must be an integer an error will be thrown and PhantomJS will exit.
+
+String inputs that are enclosed in single or double quotes will be returned as one string.  A string following an argument flag that is not enclosed in quotes will be returned as a comma separated string.
+
+Arguments that do not have a type specifier will return as true or undefined/false.
+
+For instance phantomjs file.js r will return true when tested for the presence of 'r'.
+
+
+Examples:
+
+Include a user specified JS file.
+
+var webPage = require('webpage');
+var page = webPage.create();
+
+var injectedJS = phantom.injectJs('argparse.js');
+
+var opts = [
+    'v|verbose',
+    'f|file=s'
+];
+
+var args = argparse( opts );
+
+if( args['f'] ) {
+    var userJS = phantom.injectJs( args['f'] );
+}
+
+
+
+Print out a help statement
+
+var webPage = require('webpage');
+var page = webPage.create();
+
+var injectedJS = phantom.injectJs('argparse.js');
+
+var opts = [
+    'v|verbose',
+    'f|file=s',
+    'h|help'
+];
+
+var args = argparse( opts );
+
+if( args['h'] ) {
+    console.log('Help: \nUsage:phantomjs <script>.js <args = v|verbose, f|files, h|help>');
+    phantom.exit();
+}
--- argparse.js
+++ argparse.js
@@ -1,71 +1,88 @@
-'use strict';
+var argparse = function( opts ) {
+    
+    var opt_list = {};
+    var opt_types = {};
+    var all_opts = [];
+    var args = {};
+    var OPTIONS = require('system').args
 
-class argparse {
-    constructor( opts ){
-        this.parseOpts( opts );
-    }
-    
-    
-    parseOpts( opts ) {
-        var opt_list = {};
-        var args = {};
-        
-        // PARSE OUT ALL POSSIBLE OPTIONS
-        opts.forEach( function(a) {
-            var tmp_split = a.split('|');
-            opt_list[ tmp_split[0] ] = {};
-            
-            tmp_split.forEach(function(aa) {
-                opt_list[ tmp_split[0] ][ aa.split('=')[0] ] = a.split('=')[1];
-            }); 
-        });
-        
-        
-        console.log( opt_list );
-        console.log( args );
-        return;
-        
-        
-        Object.keys(opt_list).forEach(function(a) {
-            var value;
-            console.log( opt_list[a]);
-            
-            Object.keys( opt_list[a] ).forEach(function(b) {
-                console.log( b );
-                
-                if( OPTIONS.indexOf(b) !== -1 ) {
+    // PARSE OUT ALL POSSIBLE OPTIONS
+    opts.forEach( function(a) {
+        var tmp_split = a.split('|');
+        opt_list[ tmp_split[0] ] = {};
+
+        tmp_split.forEach(function(aa) {
+            opt_list[ tmp_split[0] ][ aa.split('=')[0] ] = a.split('=')[1];
+            opt_types[ aa.split('=')[0] ] = a.split('=')[1];
+            all_opts.push( aa.split('=')[0] );
+        }); 
+    });
+
+
+    Object.keys(opt_list).forEach(function(a) {
+
+        Object.keys( opt_list[a] ).forEach(function(b) {
+
+            if( OPTIONS.indexOf( b ) !== -1 ) {
+
+                var value;
+                if( typeof opt_list[a][b] === 'undefined' ) {
                     value = true;
-                    console.log(b);
-                } 
-            });
-            
+                } else {
+                    var idx = OPTIONS.indexOf(b);
+                    var type = opt_list[a][b];
+
+                    var v = [];
+                    for( var i = idx + 1 ; i < OPTIONS.length; i++ ) { 
+
+                        if( all_opts.indexOf( OPTIONS[i] ) === -1 ) {                
+                            v.push( OPTIONS[i] );
+                        } else {
+                            break;
+                        }
+                    }
+
+                    if( v.length == 0 ) { 
+                        console.log( 'ERROR: No value given for ' + b + '.');
+                        phantom.exit();
+                    } else {
+                        value = v.join();
+                    }
+
+                }
+
+                Object.keys( opt_list[a] ).forEach(function(c) {
+                    if( value === true ) {
+                        args[c] = value;
+
+                    } else {
+                        if( opt_types[c] === 's' ) {
+                            args[c] = value;
+                        } else if( opt_types[c] === 'i' ) {
+
+                            if( isNaN( parseInt(value) )) {
+                                console.log( 'ERROR: Input type is not a required integer.');
+                                phantom.exit();
+                            } else {
+                                args[c] = parseInt(value);
+                            }
+
+                        } else {
+                            console.log( 'ERROR: Unknown value type does not match the required type (' + opt_types[c] + ') for ' + c );
+                            phantom.exit();
+                        }
+
+                    }
+
+                });
+
+            }
+
         });
-        
-        return;
-        
-        Object.keys(opt_list).forEach(function(o) {
-            if( OPTIONS.indexOf( o ) !== -1 ) { args[o] = true }    
-        });
-        
-        console.log( opt_list );
- 
-        
-        console.log( args );
-        return;
-    }
+
+    });
+
+    return args;
     
-    
+};
 
-}
-
-var opts = [
-    'v|verbose',
-    'n|name=s',
-    'h|help',
-    'int|integer=i',
-    'bugger'
-];
-
-var OPTIONS = ['v', 'name', 'int', 'bugger'];
-
-var argP = new argparse( opts );
